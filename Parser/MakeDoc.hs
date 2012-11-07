5d5d6d5f008baf690c83f2af974f71cad0d78a28
Rickard Lindberg and Rasmus Svensson <ricli85+raek@gmail.com>
2012-11-07 19:19:24 +0100

    Extract "doc" construction functions to a new file

---
+++ Parser/MakeDoc.hs
@@ -0,0 +1,34 @@
+module Parser.MakeDoc where
+
+import qualified Data.Map as M
+import Parser.Doc
+
+makeEncodeCase :: String -> [Char] -> [[Char]] -> EncodeCase
+makeEncodeCase name arguments wordBits = EncodeCase name arguments wordSpecs
+    where
+      wordSpecs = map makeWordSpec wordBits
+
+makeWordSpec :: [Char] -> WordSpec
+makeWordSpec bits = WordSpec shiftOperations
+    where
+      sources         = makeSources bits (initialLeft bits M.empty)
+      bitValues       = zipWith makeBitValue bits sources
+      destinations    = [15, 14..]
+      shiftOperations = zipWith ShiftOperation bitValues destinations
+
+      makeSources :: [Char] -> M.Map Char Int -> [Int]
+      makeSources []     _    = []
+      makeSources (b:bs) left =
+        let value = (left M.! b) - 1
+            rest  = makeSources bs (M.adjust (subtract 1) b left)
+        in  value:rest
+
+      initialLeft :: [Char] -> M.Map Char Int -> M.Map Char Int
+      initialLeft []     left = left
+      initialLeft (b:bs) left = initialLeft bs $ M.insertWith (+) b 1 left
+
+      makeBitValue :: Char -> Int -> BitValue
+      makeBitValue '0' _    = Zero
+      makeBitValue '1' _    = One
+      makeBitValue b source = Argument b source
+
