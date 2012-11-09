2d7d9ecea3a5384f62e078d7e566de04fce9b0ed
Rickard Lindberg <ricli85@gmail.com>
2012-11-09 11:43:18 +0100

    format code differently

--- Parser/Doc.hs
+++ Parser/Doc.hs
@@ -1,27 +1,16 @@
 module Parser.Doc where
 
-data Doc = Doc
-    [Constructor] [EncodeCase] [DecodeCase]
+data Doc            = Doc [Constructor] [EncodeCase] [DecodeCase]
 
-data Constructor = Constructor
-    Name Arguments
+data Constructor    = Constructor Name Arguments
 
-data EncodeCase = EncodeCase
-    Name Arguments [WordSpec]
+data EncodeCase     = EncodeCase Name Arguments [WordSpec]
+data WordSpec       = WordSpec [ShiftOperation]
+data ShiftOperation = ShiftOperation BitValue Int
+data BitValue       = Zero | One | Argument Argument Int
 
-data WordSpec = WordSpec
-    [ShiftOperation]
+data DecodeCase     = DecodeCase
 
-data ShiftOperation = ShiftOperation
-    BitValue Int
-
-data BitValue = Zero | One | Argument Argument Int
-
-data DecodeCase = DecodeCase
-
-type Name = String
-
-type Arguments = [Argument]
-
-type Argument = Char
-
+type Name           = String
+type Arguments      = [Argument]
+type Argument       = Char
