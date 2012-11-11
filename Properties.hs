3f19009478dc0c42bf31ac6d666c4a16ea837b06
Rickard Lindberg <ricli85@gmail.com>
2012-11-11 10:52:20 +0100

    normalize equivalent instructions

--- Properties.hs
+++ Properties.hs
@@ -3,9 +3,11 @@ import Test.QuickCheck
 import Instructions
 
 prop_encodeDecode :: Instruction -> Bool
-prop_encodeDecode randomInstruction =
-    let (decodedInstruction, []) = decode (encode randomInstruction)
-    in  decodedInstruction == randomInstruction
+prop_encodeDecode x = normalize x' == normalize x
+    where
+        (x', []) = decode (encode x)
+        normalize (BRBC 0 k) = BRCC k
+        normalize x          = x
 
 main = do
     quickCheck prop_encodeDecode
